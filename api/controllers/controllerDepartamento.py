86ce3eeaa74046b3a52ca2e3636ec52cea03c283
Abraham <yabraham180@gmail.com>
2022-09-24 00:23:27 -0600

    se agrego post para departamentos

--- api/controllers/controllerDepartamento.py
+++ api/controllers/controllerDepartamento.py
@@ -1,3 +1,4 @@
+from ast import operator
 import json
 from django.shortcuts import render
 from django.views import View
@@ -26,4 +27,23 @@ class DepartamentoView(View):
                     datos = { 'message': 'success', 'quantity': len(_departamento), 'data': _departamento }
         return JsonResponse(datos)
                     
-            
+    def post(self, request):
+        # RESPUESTA POR DEFECTO
+        datos = { 'message': 'fail', 'quantity': 0, 'data': [] }
+        # LEEMOS LOS DATOS ENVIADOS POR EL USUARIO
+        jd = json.loads(request.body)
+        _nombre=jd['nombre']
+        _abreviatura=jd['abreviatura']
+        print(_nombre == '')
+        if _nombre == '' or _abreviatura == '':
+            datos = { 'message': 'existen campos vacios', 'quantity': 0, 'data': [] }
+            return JsonResponse(datos)
+        departamento.objects.create(nombre=_nombre,abreviatura=_abreviatura)
+        datos = {
+                'message': 'success',
+                'data': {
+                    'nombre' : _nombre,
+                    'abreviatura': _abreviatura
+                }
+            }
+        return JsonResponse(datos)
