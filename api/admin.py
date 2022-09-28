a91433bb0a277006e2efe7bdddce6707e1f1db2b
josepcatun <josepcatun@gmail.com>
2022-09-27 22:01:37 -0600

    creacion de tabla platillos

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from api.models.modelPlatillos import platillos
+
 from .models.modelTipoUsuario import tipousuario
 from .models.modelDepartamento import departamento
 from .models.modelCategoria import categoria
@@ -12,3 +14,4 @@ admin.site.register(departamento)
 admin.site.register(categoria)
 admin.site.register(municipio)
 admin.site.register(usuario)
+admin.site.register(platillos)
---
+++ api/models/modelPlatillos.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from .modelUsuario import usuario
+from .modelCategoria import categoria
+
+class platillos(models.Model):
+    descripcion=models.CharField(max_length=200)
+    precio=models.FloatField(max_length=200)
+    id_usuario=models.ForeignKey(usuario, on_delete=models.DO_NOTHING)
+    id_categoria=models.ForeignKey(categoria, on_delete=models.DO_NOTHING)
+    stock=models.IntegerField(max_length=11)
+    estado=models.IntegerField(max_length=11,default=1)
