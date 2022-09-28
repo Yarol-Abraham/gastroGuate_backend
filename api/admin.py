70b5876cd504d491343d7909692f79b9d3b7ff7e
Abraham <yabraham180@gmail.com>
2022-09-27 23:35:47 -0600

    creacion de modelo para pedidos

--- api/admin.py
+++ api/admin.py
@@ -7,6 +7,7 @@ from .models.modelDepartamento import departamento
 from .models.modelCategoria import categoria
 from .models.modelMunicipio import municipio
 from .models.modelUsuario import usuario
+from .models.modelPedidos import pedidos
 
 # Register your models here.
 admin.site.register(tipousuario)
@@ -15,3 +16,4 @@ admin.site.register(categoria)
 admin.site.register(municipio)
 admin.site.register(usuario)
 admin.site.register(platillos)
+admin.site.register(pedidos)
--- api/models/modelPedidos.py
+++ api/models/modelPedidos.py
@@ -1,1 +1,22 @@
 from django.db import models
+from .modelUsuario import usuario
+from .modelPlatillos import platillos
+
+class pedidos(models.Model):
+    descripcion=models.CharField(max_length=200)
+    cantidad=models.IntegerField(max_length=11,default=1)
+    total=models.FloatField(max_length=200)
+    id_usuario=models.ForeignKey(usuario, on_delete=models.DO_NOTHING)
+    id_platillo=models.ForeignKey(platillos, on_delete=models.DO_NOTHING)
+    ubicacion=models.CharField(max_length=200)
+    fecha=models.DateTimeField(auto_now_add=True)
+    class ESTADOPEDIDO(models.TextChoices):
+        PROCESO = 'p'
+        ENVIADO = 'E'
+        ENTREGADO = 'ET'
+    estado_pedido = models.CharField(
+        max_length=2,
+        choices=ESTADOPEDIDO.choices,
+        default=ESTADOPEDIDO.PROCESO,
+    )
+    estado=models.IntegerField(max_length=11,default=1)
